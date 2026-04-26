8035d2fb080c37e4566f2d3e9ca06daa4be06807
yelasri <elasriyoussef604@gmail.com>
2026-04-26 12:13:03 +0100

    refactor: remove unused spec files and enhance home page with navigation and animations

--- src/app/features/home-page/home-page.ts
+++ src/app/features/home-page/home-page.ts
@@ -1,16 +1,18 @@
-import { Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { CaptchaStateService } from '../../shared/services/captcha-state.service';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
 export class HomePage {
   private captchaState = inject(CaptchaStateService)
   private router = inject(Router)
+
+  isStagesDone = signal(this.captchaState.getIsStagesDone())
 
   navigateToCaptchaPage(level?: number) {
     if (level) this.captchaState.setLevel(level)
